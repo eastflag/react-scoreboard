2fc75afb8d05799da8956d52bf115b810cb6f9de
eastflag <dklee98@>
2021-06-04 18:01:28 +0900

    function component 4

--- scoreboard-page/app.js
+++ scoreboard-page/app.js
@@ -1,17 +1,22 @@
-const Header = (props) => (<div className="header d-flex justify-content-between align-items-center p-2">
-  <span>Total Score: 0</span>
-  <h1>My Scoreboard</h1>
-  <span>players: 0</span>
-</div>);
+const Header = (props) => {
+  console.log(props);
+  // props는 read only 이다. 아래와 같이 하면 안된다.
+  // props.totalPlayers = 12;
+  return (<div className="header d-flex justify-content-between align-items-center p-2">
+    <span>Total Score: 0</span>
+    <h1>{props.title}</h1>
+    <span>players: {props.totalPlayers}</span>
+  </div>)
+};
 
 const Player = (props) => (
   <div className="container">
     <div className='player row align-items-center'>
       <div className="col-9">
-        <span>LDK</span>
+        <span>{props.name}</span>
       </div>
       <div className="col-3 counter">
-        <Counter />
+        <Counter score={props.score} />
       </div>
     </div>
   </div>
@@ -19,14 +24,16 @@ const Player = (props) => (
 
 const Counter = (props) => (<div className='d-flex justify-content-between align-items-center'>
   <button className='btn btn-info' > - </button>
-  <span>0</span>
+  <span>{props.score}</span>
   <button className='btn btn-info'> + </button>
 </div>)
 
 const App = (props) => (
   <div className="container p-3">
-    <Header/>
-    <Player/>
+    <Header title="My Scoreboard" totalPlayers={11}/>
+
+    {/*Players List*/}
+    { props.initialPlayers.map(item => <Player name={item.name} score={item.score} key={item.id} />) }
   </div>);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App initialPlayers={players} />, document.getElementById('root'));
