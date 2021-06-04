d19bf6d0dcf4fccb391653ea97cca8c70e7dde47
eastflag <dklee98@>
2021-06-04 16:20:51 +0900

    function component 1

--- scoreboard-page/app.js
+++ scoreboard-page/app.js
@@ -1,9 +1,9 @@
-const strTitle = 'This is a title';
+function Header(props) {
+  return (<div className="header d-flex justify-content-between align-items-center p-2">
+    <span>Total Score: 0</span>
+    <h1>My Scoreboard</h1>
+    <span>players: 0</span>
+  </div>);
+}
 
-const header = (<div className="header d-flex justify-content-between align-items-center p-2">
-  <span>Total Score: 0</span>
-  <h1 title={strTitle}>My Scoreboard</h1>
-  <span>players: 0</span>
-</div>);
-
-ReactDOM.render(header, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('root'));
