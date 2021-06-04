ac30f74254af3334723a8cfcce4172d695e71574
eastflag <dklee98@>
2021-06-04 12:52:44 +0900

    .

--- scoreboard-page/app.js
+++ scoreboard-page/app.js
@@ -1,6 +1,6 @@
 const header = (<div className="header d-flex justify-content-between align-items-center p-2">
   <span>Total Score: 0</span>
-  <span>Total Score: 0</span>
+  <h1 title="This is a title">My Scoreboard</h1>
   <span>players: 0</span>
 </div>);
 
