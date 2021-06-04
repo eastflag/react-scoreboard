dddca4e5d96cda3cf394c384950cf085f6d8aefc
eastflag <dklee98@>
2021-06-04 12:25:16 +0900

    jsx 3

--- scoreboard-page/app.js
+++ scoreboard-page/app.js
@@ -1,12 +1,7 @@
-const title = <h1 title="This is a title">My Scoreboard</h1>;
-console.log(title);
-
-const totalScore = <span>Total Score: 0</span>;
-
-const players = <span>players: 0</span>;
-
 const header = (<div className="header d-flex justify-content-between align-items-center p-2">
-  {[totalScore, title, players]}
+  <span>Total Score: 0</span>
+  <span>Total Score: 0</span>
+  <span>players: 0</span>
 </div>);
 
 ReactDOM.render(header, document.getElementById('root'));
