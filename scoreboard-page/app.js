9862479eb8d42c86b0284d8d9cdd416fa44b28c3
eastflag <dklee98@>
2021-06-04 16:22:20 +0900

    function component 2

--- scoreboard-page/app.js
+++ scoreboard-page/app.js
@@ -1,9 +1,7 @@
-function Header(props) {
-  return (<div className="header d-flex justify-content-between align-items-center p-2">
-    <span>Total Score: 0</span>
-    <h1>My Scoreboard</h1>
-    <span>players: 0</span>
-  </div>);
-}
+const Header = (props) => (<div className="header d-flex justify-content-between align-items-center p-2">
+  <span>Total Score: 0</span>
+  <h1>My Scoreboard</h1>
+  <span>players: 0</span>
+</div>);
 
 ReactDOM.render(<Header />, document.getElementById('root'));
