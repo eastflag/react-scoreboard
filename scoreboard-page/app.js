586b4580e5198a576faab6004968c1e4b5370c15
eastflag <dklee98@>
2021-06-04 16:52:37 +0900

    function component 3

--- scoreboard-page/app.js
+++ scoreboard-page/app.js
@@ -4,4 +4,29 @@ const Header = (props) => (<div className="header d-flex justify-content-between
   <span>players: 0</span>
 </div>);
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+const Player = (props) => (
+  <div className="container">
+    <div className='player row align-items-center'>
+      <div className="col-9">
+        <span>LDK</span>
+      </div>
+      <div className="col-3 counter">
+        <Counter />
+      </div>
+    </div>
+  </div>
+);
+
+const Counter = (props) => (<div className='d-flex justify-content-between align-items-center'>
+  <button className='btn btn-info' > - </button>
+  <span>0</span>
+  <button className='btn btn-info'> + </button>
+</div>)
+
+const App = (props) => (
+  <div className="container p-3">
+    <Header/>
+    <Player/>
+  </div>);
+
+ReactDOM.render(<App />, document.getElementById('root'));
