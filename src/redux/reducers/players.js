16b9a1325d4a70ad8ea354cb71d7c5b7b5fe6240
eastflag <eastflag@gmail.com>
2021-06-13 04:44:46 +0900

    redux 구조 적용

---
+++ src/redux/reducers/players.js
@@ -0,0 +1,11 @@
+const playerInitialState = {
+  players: [],
+};
+
+export const playerReducer = (state = playerInitialState, action) => {
+
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
