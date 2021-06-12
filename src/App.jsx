bff71c8887bad8f3a5d1f563ed9492d07d77bbaf
eastflag <eastflag@gmail.com>
2021-06-13 03:50:21 +0900

    훅으로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Header} from "./components/Header";
@@ -7,89 +7,76 @@ import axios from 'axios';
 import {CustomPlayer} from "./components/CustomPlayer";
 import _ from 'lodash';
 
-class App extends React.Component {
-  state = {
-    players: []
-  };
+const App = (props) => {
+  const [players, setPlayers] = useState([]);
 
-  // 화면 렌더링 직후에 자동으로 호출되는 라이프사이클 메서드
-  componentDidMount() {
+  useEffect(() => {
     axios.get('http://api.eastflag.co.kr:8000/api/score/list')
       .then(response => {
         console.log(response);
         const {data} = response;
-        this.setState({players: data});
+        setPlayers(data);
       });
-  }
+  }, [])
 
-  handleRemovePlayer = (id) => {
+  const handleRemovePlayer = (id) => {
     axios.delete(`http://api.eastflag.co.kr:8000/api/score?id=${id}`)
       .then(response => {
         console.log(response);
         const {data} = response;
         if (data.result === 0) {
-          this.setState(prevState => {
-            return {
-              players: prevState.players.filter(item => item.id !== id)
-            }
-          })
+          setPlayers(players.filter(item => item.id !== id));
         }
       });
   }
 
-  handleChangeScore = (id, delta) => {
+  const handleChangeScore = (id, delta) => {
     console.log('id: ' + id, 'delta: ' + delta);
-    this.setState(prevState => {
-      // 새로운 players 배열 생성
-      const players = [ ...prevState.players ];
-      players.forEach(player => {
-        if (player.id === id) {
-          player.score += delta;
-        }
-      })
-      return { players }
+
+    const copyPlayers = [ ...players ];
+    copyPlayers.forEach(player => {
+      if (player.id === id) {
+        player.score += delta;
+      }
     })
+    setPlayers(copyPlayers);
   }
 
-  handleAddPlayer = (name) => {
+  const handleAddPlayer = (name) => {
     console.log(name);
     axios.post('http://api.eastflag.co.kr:8000/api/score', {name})
       .then(response => {
         console.log(response);
         const {data} = response;
 
-        this.setState(prevState => {
-          const players = [ ... prevState.players ];
-          players.unshift(data);
-          return { players };
-        });
+        const copyPlayers = [ ... players ];
+        copyPlayers.unshift(data);
+        setPlayers(copyPlayers);
       });
   };
 
-  getHighScore() {
-    const maxObject = _.maxBy(this.state.players, 'score');
+  const getHighScore = () => {
+    const maxObject = _.maxBy(players, 'score');
     const highScore = maxObject.score;
     // 0은 디폴트이므로  0보다 클 경우만 highScore로 지정한다.
     return highScore > 0 ? highScore : null;
   }
 
-  render() {
-    return (
-      <div className="container p-3">
-        <Header title="My scoreboard" players={this.state.players} />
+  return (
+    <div className="container p-3">
+      <Header title="My scoreboard" players={players} />
 
-        {/*Players List*/}
-        { this.state.players.map(item =>
-          <CustomPlayer key={item.id}
-                  name={item.name} score={item.score} id={item.id}
-                  isHighScore={item.score === this.getHighScore()}
-                  removePlayer={this.handleRemovePlayer}
-                  changeScore={this.handleChangeScore} />) }
+      {/*Players List*/}
+      { players.map(item =>
+        <CustomPlayer key={item.id}
+                      name={item.name} score={item.score} id={item.id}
+                      isHighScore={item.score === getHighScore()}
+                      removePlayer={handleRemovePlayer}
+                      changeScore={handleChangeScore} />) }
 
-        <AddPlayerForm addPlayer={this.handleAddPlayer}></AddPlayerForm>
-      </div>
-    );
-  }
+      <AddPlayerForm addPlayer={handleAddPlayer}></AddPlayerForm>
+    </div>
+  )
 }
 
 export default App;
--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -1,25 +1,19 @@
-import React, {Component} from 'react';
+import React, {useRef, useState} from 'react';
 
-export class AddPlayerForm extends Component {
-  state = {
-    value: ''
+export const AddPlayerForm = (props) => {
+  const formRef = useRef();
+  const textRef = useRef();
+  const [value, setValue] = useState('');
+
+  const handleValueChange = (e) => {
+    setValue(e.target.value);
   };
 
-  constructor(props) {
-    super(props);
-    this.formRef = React.createRef();
-    this.textRef = React.createRef();
-  }
-
-  handleValueChange = (e) => {
-    this.setState({value: e.target.value});
-  };
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    const form = this.formRef.current; // form node
-    const player = this.textRef.current; // input node
+    const form = formRef.current; // form node
+    const player = textRef.current; // input node
 
     console.log(player.validity.valid);
     console.log(form.checkValidity());
@@ -29,33 +23,30 @@ export class AddPlayerForm extends Component {
       return;
     }
 
-    this.props.addPlayer(this.state.value);
-    this.setState({
-      value: ''
-    });
+    props.addPlayer(value);
+    setValue('')
+
     form.classList.remove('was-validated');
   }
 
-  render() {
-    return (
-      <div className="container">
-        <form ref={this.formRef} noValidate
-              className="row player align-items-center needs-validation" onSubmit={this.handleSubmit}>
-          <div className="col-9">
-            {/*<label htmlFor="playerName" className="form-label">Player Name</label>*/}
-            <input ref={this.textRef}
-                   type="text" className="form-control" id="playerName" placeholder="input player name"
-                   required
-                   value={this.state.value} onChange={this.handleValueChange}></input>
-            <div className="invalid-feedback">
-              Please input name.
-            </div>
+  return (
+    <div className="container">
+      <form ref={formRef} noValidate
+            className="row player align-items-center needs-validation" onSubmit={handleSubmit}>
+        <div className="col-9">
+          {/*<label htmlFor="playerName" className="form-label">Player Name</label>*/}
+          <input ref={textRef}
+                 type="text" className="form-control" id="playerName" placeholder="input player name"
+                 required
+                 value={value} onChange={handleValueChange}></input>
+          <div className="invalid-feedback">
+            Please input name.
           </div>
-          <div className="col-3 d-grid">
-            <button type="submit" className="btn btn-primary">Add Player</button>
-          </div>
-        </form>
-      </div>
-    )
-  }
+        </div>
+        <div className="col-3 d-grid">
+          <button type="submit" className="btn btn-primary">Add Player</button>
+        </div>
+      </form>
+    </div>
+  )
 }
